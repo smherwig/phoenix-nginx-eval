bb74f2aa5861a5f32fc26d3da34038342c260c72
smherwig <smherwig@cs.umd.edu>
2019-08-16 14:16:05 -0400

    Add script to download and build ModSecurity.

---
+++ bin/get_modsecurity.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+# Download and build ModSecurity 3.0.
+#
+# Building with curl brings in a lot of shared library dependencies.  It seems
+# that Graphene has a bug when running the init (i.e., the consturctor) for one
+# of these libraries; however, I have not been able to isolate the bug just
+# yet.
+#
+# ModSecurity is installed to /usr/local/modsecurity.
+#
+git clone --depth 1 -b v3/master --single-branch \
+    https://github.com/SpiderLabs/ModSecurity
+
+
+cd ModSecurity
+git submodule init
+git submodule update
+./build.sh
+./configure --without-curl
+make
+sudo make install
+
+#
+# Download and ModSecurity-Nginx connector.
+#
+# (The connector is specified in NGINX's configure command and compiled as part
+# of the NGINX compilation.)
+#
+git clone --depth 1 \
+    https://github.com/SpiderLabs/ModSecurity-nginx.git
+
